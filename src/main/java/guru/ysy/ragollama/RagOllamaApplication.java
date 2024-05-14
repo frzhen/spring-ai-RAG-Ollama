fa9e7a49370e7550c9b1734618420ebf7de1728b
Fred Zhen <fred.zhen@gmail.com>
2024-05-14 16:18:06 +0800

    INIT commit

---
+++ src/main/java/guru/ysy/ragollama/RagOllamaApplication.java
@@ -0,0 +1,13 @@
+package guru.ysy.ragollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagOllamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagOllamaApplication.class, args);
+    }
+
+}
